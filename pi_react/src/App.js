c59eda6372918b2669823444560fe0dcacd8f523
Aleandro Alves <aleandroalves@outlook.com>
2025-08-11 21:50:25 -0300

    for

--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -24,8 +24,8 @@ import './App.css';
 //   );
 // }
 
-function App(){
-  function calculadora(a,b,op){
+// function App(){
+  // function calculadora(a,b,op){
     // if(op=="+"){
     //   return a + b
     // }
@@ -38,31 +38,56 @@ function App(){
     // else if(op =="*"){
     //   return a * b;
     // }
-    switch(op){
-      case "+":
-        return a + b
-      case "-":
-        return a - b
-      case "*":
-        return a * b
-      case "/":
-        return a / b      
+  //   switch(op){
+  //     case "+":
+  //       return a + b
+  //     case "-":
+  //       return a - b
+  //     case "*":
+  //       return a * b
+  //     case "/":
+  //       return a / b      
         
       
-    }
+  //   }
+  // }
+
+  // return (
+  //   <main className='App'>
+  //     {calculadora(9, calculadora(8, 7, "*"),"+")}<br/>
+  //     {calculadora(10, 2,"-")}<br/>
+  //     {calculadora(1, 2,"/")}<br/>
+  //     {calculadora(1, 2,"*")}<br/>
+
+  //   </main>
+  // );
+// }
+
+function App() {
+
+  let cesta = ["Pão", "Suco", "Chocolate", "Torrone."]
+  let desenha = [];
+  
+ // declara o indice; compara se é para continuar; incremento o indice
+ for(let i=0; i<3 ; i++){
+  desenha.push(<p> {cesta[i]} </p>)
+
+
+            
   }
-
+ 
   return (
-    <main className='App'>
-      {calculadora(9, calculadora(8, 7, "*"),"+")}<br/>
-      {calculadora(10, 2,"-")}<br/>
-      {calculadora(1, 2,"/")}<br/>
-      {calculadora(1, 2,"*")}<br/>
+    <main className="App">
+      {
+       desenha
+      }
+      
 
+        
+     
     </main>
   );
-
-
 }
+
 
 export default App;
