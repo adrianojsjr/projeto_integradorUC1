19ca53ab90623589756624c49819e57a0f489916
Mariana <marianaamelia@icloud.com>
2025-08-12 19:37:23 -0300

    Atividade numeros impares

--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -81,25 +81,47 @@ import './App.css';
 //   );
 // }
 
+// function App() {
+
+//   let cesta = ["pão", "suco", "chocolate", "torrone"];
+//   let desenha = [];
+
+// //i=0 -> primeira posição do vetor. No caso, a primeira posição é o pão (casa 0)
+// //i<3 -> decide que o vetor vai contar somente até dois, ou seja, vai exibir somente até o chocolate
+// //i++ -> soma mais um na posição do vetor
+//   for(let i=0; i<3; i++){
+//     desenha.push(<p> { cesta[i] } </p>)
+//   }
+
+//   return (
+//     <main className="App">
+//       {desenha}
+//     </main>
+//   );
+// }
+
 function App() {
 
-  let cesta = ["pão", "suco", "chocolate", "torrone"];
-  let desenha = [];
+  //faça uma lista com todos os numeros ate 10, mas exiba apenas os numeros impares
 
-//i=0 -> primeira posição do vetor. No caso, a primeira posição é o pão (casa 0)
-//i<3 -> decide que o vetor vai contar somente até dois, ou seja, vai exibir somente até o chocolate
-//i++ -> soma mais um na posição do vetor
-  for(let i=0; i<3; i++){
-    desenha.push(<p> { cesta[i] } </p>)
+  let numero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  let impar = [];
+
+  for (let i = 0; i < numero.length; i++) { // i começa em 0 e vai até o último índice (numero.length).
+    if (numero[i] % 2 !== 0) {  //verifica se o resto é diferente de 0
+      impar.push(<p>{numero[i]}</p>); //Se for ímpar, adiciona um <p> com o número dentro de impares
+    }
   }
 
-  return (
-    <main className="App">
-      {desenha}
-    </main>
-  );
+
+
+
+return (
+  <main className="App">
+    {impar}
+  </main>
+);
+
 }
-
-
 
 export default App;
