afc01ed0954c670dbd22707e8bb54470500b9bb4
adrianojsjr <adrianojrsilva1@gmail.com>
2025-08-11 21:17:16 -0300

    switch feito

--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -4,29 +4,24 @@ import './App.css';
 function App() { // Aqui é JavaScript
 
   function calculadora(a, b, op) {
-    if (op == '+') {
-      return a + b;
-    }
-
-    else if (op == '-') {
-      return a - b;
-    }
-
-    else if(op == '/'){
-      return a /b;
-    }
-
-    else if (op == '*') {
-      return a *b;
+    switch (op) {
+      case '+':
+        return a + b
+      case '-':
+        return a - b
+      case '*':
+        return a * b
+      case '/':
+        return a / b
     }
   }
 
   return ( /* aqui é HTML*/
     <main className="App">
-      {calculadora(1, calculadora(2, 1, '*'),'+')}<br />
-      {calculadora(5,1, '-')}<br />
-      {calculadora(10,2, '/')}<br />
-      {calculadora(5,2, '*')}<br />
+      {calculadora(1, calculadora(2, 1, '*'), '+')}<br />
+      {calculadora(5, 1, '-')}<br />
+      {calculadora(10, 2, '*')}<br />
+      {calculadora(5, 2, '/')}<br />
     </main>
 
 
