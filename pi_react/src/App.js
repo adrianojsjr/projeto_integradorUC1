c7f91a1f8a5874fda2b5757e936f0f37132d95d1
adrianojsjr <adrianojrsilva1@gmail.com>
2025-08-11 22:15:14 -0300

    atividade

--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -2,24 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() { // Aqui é JavaScript
+  let numero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]
+  let impares = [];
 
-  let cesta = ['pão', 'suco', 'chocolate', 'torrone']
-
-  let desenha = [];
-  //declara o índice; compara se é para continuar; incrementa o índice
-  for( let i=0; i<3 ; i++){
-    
-    desenha.push(<p> {cesta[i] } </p>)
+  for (let i = 0;  i<numero.length; i++) {
+    if (numero[i] % 2 !== 0){ 
+      impares.push(<p>{numero[i]}</p>);
+    }
   }
- 
+
+
+
 
   return ( /* aqui é HTML*/
     <main className="App">
-    
-    {desenha}  
-      
-       
-     
+      {impares}
+
+
+
     </main>
 
 
