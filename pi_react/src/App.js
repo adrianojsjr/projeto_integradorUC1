49178efd50f93d06ff06edfc9db6d04cb16c0024
Mariana Amelia Rocha <marianaamelia@icloud.com>
2025-08-12 17:53:33 -0300

    Adicionei comentários

--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -5,26 +5,16 @@ function App() { // Aqui é JavaScript
   let numero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]
   let impares = [];
 
-  for (let i = 0;  i<numero.length; i++) {
-    if (numero[i] % 2 !== 0){ 
-      impares.push(<p>{numero[i]}</p>);
+  for (let i = 0;  i<numero.length; i++) { // i começa em 0 e vai até o último índice (numero.length).
+    if (numero[i] % 2 !== 0){  //verifica se o resto é diferente de 0
+      impares.push(<p>{numero[i]}</p>); //Se for ímpar, adiciona um <p> com o número dentro de impares
     }
   }
-
-
-
 
   return ( /* aqui é HTML*/
     <main className="App">
       {impares}
-
-
-
     </main>
-
-
-
-
   );
 }
 
