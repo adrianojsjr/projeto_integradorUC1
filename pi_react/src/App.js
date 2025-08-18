7c5e7a0c416153e746a104fc09cf1afd1633e74f
adrianojsjr <adrianojrsilva1@gmail.com>
2025-08-18 20:25:48 -0300

    aula 16/08

--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -1,208 +1,62 @@
-import logo from './logo.svg';
+
 import './App.css';
+import User from './User'
+import { BrowserRouter as Router, Routes, Route, Navigate, link, outlet }
+  from 'react-router-dom';
 
-// function App() {
 
-//   // let oi = "Olá, mundo";
 
-//   // oi += " Mariana";
-
-//   // function soma(a, b) {
-//   //   return a + b 
-//   // }
-
-//   // function divide (a, b){
-//   //   return a/b;
-//   // }
-
-//   return (
-//     <main className="App">
-//       {soma(oi , " Mariana")} <br/>
-//       {divide(36, 6)}
-//     </main>
-//   );
-// }
-
-
-// function App() {
-
-//   function calculadora(a, b, op) {
-//     if (op == "+") {
-//       return a + b;
-//     }
-//     else if (op == "-") {
-//       return a - b;
-//     }
-//     else if (op == "/"){
-//       return a/b;
-//     }
-//     else if (op == "*"){
-//       return a*b;
-//     }
-
-//   }
-
-//   return (
-//     <main className="App">
-//       {calculadora(10, calculadora(5, 1, "*"), "+")}<br/> 
-//       {calculadora(50, 2, "-")}<br/>
-//       {calculadora(510, 3, "/")}<br/>
-//       {calculadora(784, 6, "*")}
-//     </main>
-//   );
-// }
-
-// function App() {
-
-//   function calculadora(a, b, op) {
-//     switch (op) {
-//       case "+":
-//         return a + b
-
-//       case "-":
-//         return a - b
-
-//       case "*":
-//         return a * b
-
-//       case "/":
-//         return a / b
-//     }
-
-//   }
-
-//   return (
-//     <main className="App">
-//       {calculadora(10, calculadora(5, 1, "*"), "+")}<br />
-//       {calculadora(50, 2, "-")}<br />
-//       {calculadora(510, 3, "/")}<br />
-//       {calculadora(784, 6, "*")}
-//     </main>
-//   );
-// }
-
-// function App() {
-
-//   let cesta = ["pão", "suco", "chocolate", "torrone"];
-//   let desenha = [];
-
-// //i=0 -> primeira posição do vetor. No caso, a primeira posição é o pão (casa 0)
-// //i<3 -> decide que o vetor vai contar somente até dois, ou seja, vai exibir somente até o chocolate
-// //i++ -> soma mais um na posição do vetor
-//   for(let i=0; i<3; i++){
-//     desenha.push(<p> { cesta[i] } </p>)
-//   }
-
-//   return (
-//     <main className="App">
-//       {desenha}
-//     </main>
-//   );
-// }
-
-// function App() {
-
-//   //faça uma lista com todos os numeros ate 10, mas exiba apenas os numeros impares
-
-//   let numero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-//   let impar = [];
-
-//   for (let i = 0; i < numero.length; i++) { // i começa em 0 e vai até o último índice (numero.length).
-//     if (numero[i] % 2 !== 0) {  //verifica se o resto é diferente de 0
-//       impar.push(<p>{numero[i]}</p>); //Se for ímpar, adiciona um <p> com o número dentro de impares
-//     }
-//   }
-
-
-
-
-// return (
-//   <main className="App">
-//     {impar}
-//   </main>
-// );
-
-// }
-
-
-
-function App() {
-
-  //variável para o email
-  let email
-
-  function mudaEmail(valor) {
-
-    email = valor
-
-  }
-
-
-  //variável para senha
-  let senha
-
-  function mudaSenha(valor) {
-
-    senha = valor
-
-  }
-
-  function enviar() {
-    alert("Email: " + email + " senha: " + senha)
-  }
-
-  let telaLogin = true;
-
-  function mudaTela(){
-    telaLogin = !telaLogin
-  }
-
-  return (
-    <main className="App">
-
-      <button onClick={() => mudaTela()}>
-        {telaLogin && ("Cadastrar-se")}
-        {!telaLogin && ("Login")}
-      </button>
-
-      {/* formulário de cadastro com o campo para email, senha e um botão para enviar */}
-
-      { !telaLogin && (
-        <form className='cadastro'>
-        </form>
-      )}
-
-
-      {/* formulário de login com o campo para email, senha e um botão para enviar */}
-
-      {telaLogin && (
-        <form className='login'>
-          <p>Para logar coloque as informações abaixo</p>
-
-          <label>Digite o email
-            <input type='email' placeholder='exemplo@exemplo.com' onChange={(e) => mudaEmail(e.target.value)} />
-          </label>
-
-          <br />
-          <br />
-
-          <label>Digite a senha
-            <input type='password' placeholder='senha' onChange={(e) => mudaSenha(e.target.value)} /><br />
-          </label>
-
-          <br />
-
-          <button className="btnEntrar" onClick={() => enviar()}>Entrar</button>
-
-        </form>
-      )}
-
-
-
-
-    </main>
-  );
-
+function PrivateSession() {
+  const hasSession = !!localStorage.getItem('supaSession');
+  return hasSession ? <Outlet /> : <Navigate to="/login" replace />
 }
+
+
+function App() {                  //aqui javascript
+  const hasSession = !!localStorage.getItem('supaSession');
+
+  return (                         /* Aqui html */
+    <Router>
+      <main className="App">
+        <nav>
+          {hasSession ? (
+            <>
+              <Link to="/Home">Inicial</Link>
+              <Link to="/Search">busca</Link>
+              <Link to="/MedicalPresentation">apresentação do médico.</Link>
+              <Link to="/Schedule">agendamento</Link>
+              <Link to="/Payment">pagamento</Link>
+            </>
+
+          ) : (
+            <>
+              <Link to="/Home">Inicial</Link>
+              <Link to="/Search">busca</Link>
+            </>
+          )
+          }
+
+        </nav>
+
+        <Routes>
+          {/*Rotas Públicas*/}
+          <Route path='/Search' element={<Home />} />
+          <Route path='/Login' element={<User />} />
+
+          <Route element={<PrivateSession />}>
+
+            {/*Rotas Logado*/}
+            <Route path='/Payment' element={<Register />} />
+            
+
+            </Route>
+        </Routes>
+
+      </main>
+    </Router>
+
+  );
+}
+
 
 export default App;
---
+++ pi_react/src/User.js
@@ -0,0 +1,78 @@
+import logo from './logo.svg';
+import {useState} from 'react';
+import './App.css';
+
+
+ 
+function User() {                  //aqui javascript
+  const [user, setUser] = useState({
+    email: "",
+    password:"",
+    phone:"",
+    name:""
+
+
+  });
+ 
+  
+ 
+ 
+ 
+  function enviar(){
+    alert("email:"+user.email+" senha:"+user.password)
+  }
+ 
+ 
+ 
+  //const [email, setEmail] = useState("")//
+  //const [senha, setSenha] = useState("")//
+
+  const [isLogin, setIsLogin] = useState(true)
+  
+ 
+  return (                         /* Aqui html */
+    <main className="App">
+ 
+      <button onClick={() => setIsLogin (!isLogin)} >
+        {isLogin &&("Cadastrar-se")}
+        {!isLogin &&("Voltar para o login")}
+      </button>
+     
+      {!isLogin &&(
+     <form className="register"></form>
+      )}
+ 
+      {isLogin &&(
+      <form className="login">
+ 
+        <label >Digite Seus Dados</label>
+ 
+        <br></br>
+        <label>
+          Email: <input  type='email' placeholder='Email' onChange={(e) => setUser({...user, email: e.target.value}) } ></input>
+        </label>
+ 
+        <br></br>
+        <br></br>
+        <label>
+          Senha: <input type='password' placeholder=' Senha ' onChange={(e) => setUser ({... user, password: e.target.value})}></input>
+        </label>
+ 
+        <p><button style={{background:"white", color: "black"}} type='submit' onClick={() => enviar()} >Entrar</button></p>
+      </form>
+      )}
+ 
+     
+ 
+ 
+     
+ 
+ 
+ 
+ 
+    </main>
+  );
+}
+ 
+ 
+export default User;
