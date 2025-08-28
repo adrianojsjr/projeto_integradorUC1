5035c0b9cc68b5e63169641996db68409bc26764
adrianojsjr <adrianojrsilva1@gmail.com>
2025-08-28 19:19:23 -0300

    28/08

--- pi_react/src/Views/Schedule/Index.js
+++ pi_react/src/Views/Schedule/Index.js
@@ -1,6 +1,7 @@
 import { use, useState } from 'react';
 import { createClient } from "@supabase/supabase-js";
 import { useNavigate } from 'react-router-dom';
+import CloseButton from 'react-bootstrap/CloseButton';
 
 const supabaseUrl = "https://mayrahcoiqpxrhqtcnry.supabase.co"
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1heXJhaGNvaXFweHJocXRjbnJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQzNTAzMzgsImV4cCI6MjA2OTkyNjMzOH0.8jpiw7cQHMy4KaBl5qquKBptbjfO1FqtdE7u7X2C_OU"
@@ -11,16 +12,16 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 function Schedule() {                  //aqui javascript
   const nav = useNavigate();
   const [schedule, setSchedule] = useState({
-    dateTime:"",
+    dateTime: "",
     doctor_id: ""
   })
 
   async function creatSchedule() {
     const { data: dU, error: eU } = await supabase.auth.getUser();
-   
+
     const uid = dU?.user?.uid
-   
-    if (!uid) nav("/user", {replace: true});
+
+    if (!uid) nav("/user", { replace: true });
 
     /*if (eU) nav('/user', { replace: true })
     if (!dU) nav('/user', { replace: true })
@@ -28,10 +29,10 @@ function Schedule() {                  //aqui javascript
 
     const { data, error } = await supabase
       .from('schedule')
-      .insert({...schedule, doctor_id: uid});
-      //.select();
-        
-      
+      .insert({ ...schedule, doctor_id: uid });
+    //.select();
+
+
   }
 
 
@@ -43,7 +44,7 @@ function Schedule() {                  //aqui javascript
         <button onClick={creatSchedule}>Salvar</button>
       </form>
 
-
+      <CloseButton />;
     </div>
   );
 }
