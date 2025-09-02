e5ff7d395862f6a76e2dd5f1100b64faf105fb45
adrianojsjr <adrianojrsilva1@gmail.com>
2025-09-02 19:47:22 -0300

    02/09

---
+++ pi_react/src/Componentes/Form.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+
+function Form({
+    onSubmit,
+    func,
+    title,
+    children
+}) {
+    const [loading, setLoading] = useState(false);
+    
+
+    async function send() {
+        setLoading (true)
+        var any = await func()
+        setLoading(false)
+        return any
+        
+    }
+
+
+
+    return (
+        <div className='cardForm'>
+            <h3>{title}</h3>
+            <form onSubmit={(e)=> e.preventDefault()}>
+                {children}
+
+                <button onClick={() => send()} > Salvar </button>
+            </form>
+        </div>
+    )
+
+}
+
+export { Form }
